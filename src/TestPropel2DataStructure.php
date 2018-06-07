eef54a7fae482e37743291ea701aebbc184f31d0
Mike Hughes <mikehu@inmotionhosting.com>
2018-06-07 16:10:53 -0400

    Set default database

--- src/TestPropel2DataStructure.php
+++ src/TestPropel2DataStructure.php
@@ -5,10 +5,11 @@
  */
 class TestPropel2DataStructure
 {
-	const NAME				= 'test_propel2_data_structure';
-	const SCHEMA_PATH		= __DIR__ . '/../config';
-	const CONFIG_PATH		= __DIR__ . '/../config';
-	const SQL_PATH			= __DIR__ . '/../data';
+	const NAME		= 'test_propel2_data_structure';
+	const SCHEMA_PATH	= __DIR__ . '/../config';
+	const CONFIG_PATH	= __DIR__ . '/../config';
+	const SQL_PATH		= __DIR__ . '/../data';
 	const MIGRATION_PATH	= __DIR__ . '/../data/migrations';
-	const CLASS_PATH		= __DIR__ . '/../src';
+	const CLASS_PATH	= __DIR__ . '/../src';
+        const DBNAME            = 'test';
 }
