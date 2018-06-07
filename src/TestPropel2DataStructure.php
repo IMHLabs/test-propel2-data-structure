ffb93dbaa3d5b1a2e49993cc8831c7bb64714e35
Mike Hughes <mikehu@inmotionhosting.com>
2018-06-07 13:11:59 -0400

    Initial Code commit

---
+++ src/TestPropel2DataStructure.php
@@ -0,0 +1,14 @@
+<?php
+/** 
+ * TestPropel2DataStructure
+ *
+ */
+class TestPropel2DataStructure
+{
+	const NAME				= 'test_propel2_data_structure';
+	const SCHEMA_PATH		= __DIR__ . '/../config';
+	const CONFIG_PATH		= __DIR__ . '/../config';
+	const SQL_PATH			= __DIR__ . '/../data';
+	const MIGRATION_PATH	= __DIR__ . '/../data/migrations';
+	const CLASS_PATH		= __DIR__ . '/../src';
+}
